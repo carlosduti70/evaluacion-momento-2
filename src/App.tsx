3db8323b799203760045ec4984c722a36daf0931
carlosduti70 <cmzuniga@sudamericano.edu.ec>
2023-12-23 14:47:25 -0500

    evaluacion

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Card } from 'antd';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+interface User {
+  id: number;
+  name: string;
+  email: string;
+  abilities: string[];
+  image: string;
+}
 
 function App() {
+
+  const [users, setUsers] = useState<User[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string>('');
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const usersResponse = await axios.get<User[]>('https://jsonplaceholder.typicode.com/users');
+
+        const imageUrls = [
+          'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png', // URL de la primera imagen
+          'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/005.png',
+          'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/006.png',
+          'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/009.png',
+          'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/011.png',
+          'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/012.png',
+          'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/002.png',
+          'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/003.png',
+          'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/008.png',
+          'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/010.png'
+
+           // URL de la segunda imagen
+          // Agrega más URLs de imágenes aquí según sea necesario
+        ];
+
+        const usersWithAbilities = await Promise.all(
+          usersResponse.data.map(async (user, index) => {
+            // Realiza otra solicitud para obtener las habilidades de los Pokémon
+            const abilitiesResponse = await axios.get(`https://pokeapi.co/api/v2/ability/`);
+            // Obtén los nombres de las habilidades
+            const abilities = abilitiesResponse.data.results.map((ability: any) => ability.name);
+            return { ...user, abilities, image: imageUrls[index % imageUrls.length] }; // Agrega las habilidades al usuario
+          })
+        );
+        setUsers(usersWithAbilities);
+        setLoading(false);
+      } catch (error) {
+        setError('Error al obtener datos');
+        setLoading(false);
+      }
+    };
+
+    fetchUserData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      
+      {loading && <p>Loading...</p>}
+      {error && <p>{error}</p>}
+      {users.map((user, index) => (
+        <Card
+          className="Card"
+          hoverable
+          style={{ width: '30%' }}
+          key={user.id}
+          cover={<img alt={user.name} src={user.image} />}
         >
-          Learn React
-        </a>
-      </header>
+          <Card.Meta title={user.name} description={`Power: ${user.abilities[index % user.abilities.length]}`} />
+        </Card>
+      ))}
     </div>
   );
 }
